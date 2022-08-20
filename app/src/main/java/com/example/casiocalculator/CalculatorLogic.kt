0bafac0691aa21b3ba602b30a80de73aceeb79a2
Himanshu Matharu <himanshu.matharu@graywolftechnologies.com>
2022-08-20 07:20:19 -0400

    - Initial Commit

---
+++ app/src/main/java/com/example/casiocalculator/CalculatorLogic.kt
@@ -0,0 +1,61 @@
+package com.example.casiocalculator
+
+class CalculatorLogic {
+
+    /** Variables and Properties */
+    private var number: Double? = null
+    private var intermediateCalculation: Pair<Double,String>? = null
+
+    /** Class Methods */
+    fun setNumber(number:Double){
+        this.number = number
+    }
+
+    fun calculate(symbol:String):Pair<Double?,String?>{
+        this.number?.let {
+            when (symbol) {
+                "+/-" -> {
+                    return Pair(it * -1, "")
+                }
+                "C" -> {
+                    return Pair(0.0,"")
+                }
+                "%" -> {
+                    return Pair(it * 0.01, "")
+                }
+                "=" -> {
+                    return this.performTwoNumCalculation(it)
+                }
+                else -> {
+                    this.intermediateCalculation = Pair(it,symbol)
+                }
+            }
+        }
+        return Pair(null,"")
+    }
+
+    private fun performTwoNumCalculation(n2:Double):Pair<Double?,String?>{
+        this.intermediateCalculation?.let {
+            val n1 = it.first
+            when (it.second) {
+                "+" -> {
+                    return Pair(n1+n2, "${StringUtils.trimTrailingZero(n1.toString())}+${StringUtils.trimTrailingZero(n2.toString())}")
+                }
+                "-" -> {
+                    return Pair(n1-n2, "${StringUtils.trimTrailingZero(n1.toString())}-${StringUtils.trimTrailingZero(n2.toString())}")
+                }
+                "x" -> {
+                    return Pair(n1*n2, "${StringUtils.trimTrailingZero(n1.toString())}x${StringUtils.trimTrailingZero(n2.toString())}")
+                }
+                "/" -> {
+                    return Pair(n1/n2, "${StringUtils.trimTrailingZero(n1.toString())}/${StringUtils.trimTrailingZero(n2.toString())}")
+                }
+                else -> {
+                    throw Exception("The operation passed in does not match any of the cases.")
+                }
+            }
+        }
+        return Pair(null,"")
+    }
+
+}
---
+++ app/src/main/java/com/example/casiocalculator/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.casiocalculator
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.example.casiocalculator.databinding.ActivityMainBinding
+import kotlin.math.floor
+
+class MainActivity : AppCompatActivity() {
+    /** Variables and Properties */
+    private lateinit var binding: ActivityMainBinding
+    private var displayLabel: TextView? = null
+    private var calculationLabel: TextView? = null
+
+    private var isFinishedTypingNumber: Boolean = true
+
+    private var displayValue: Double
+        get() {
+            return displayLabel?.text.toString().toDouble()
+        }
+        set(value) {
+            displayLabel?.text = StringUtils.trimTrailingZero(String.format("%.2f",value))
+        }
+
+    private var calculationValue: String
+        get() {
+            return calculationLabel?.text.toString()
+        }
+        set(value) {
+            calculationLabel?.text = value
+        }
+
+    private var calculator = CalculatorLogic()
+
+    /** Class Methods */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        displayLabel = binding.displayTv
+        calculationLabel = binding.calculationTv
+
+        displayLabel?.text = "0"
+        calculationLabel?.text = ""
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun numBtnPressed(view:View) {
+        val numValue = (view as Button).text
+        if (isFinishedTypingNumber) {
+            displayLabel?.text = numValue
+            isFinishedTypingNumber = false
+        }else{
+            if (numValue == ".") {
+                val isInt = floor(displayValue) == displayValue
+                if (!isInt) {
+                    return
+                }
+            }
+            displayLabel?.text = displayLabel?.text.toString() + numValue
+        }
+    }
+
+    fun calcBtnPressed(view:View) {
+        isFinishedTypingNumber = true
+        this.calculator.setNumber(displayValue)
+        val calcMethod = (view as Button).text
+        val result = this.calculator.calculate(calcMethod as String)
+        result.first?.let { displayValue = it }
+        result.second?.let { calculationValue = it }
+    }
+
+    fun backBtnPressed(view:View) {
+        if (!isFinishedTypingNumber) {
+            displayLabel?.text.let {
+                var newNumber = it.toString().dropLast(1)
+                if (newNumber.isEmpty()) {
+                    newNumber = "0"
+                    isFinishedTypingNumber = true
+                }
+                displayLabel?.text = newNumber
+            }
+        }
+    }
+}
